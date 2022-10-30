314c52349452a16776792aa9d3a1de0495e3d460
MatheusShirakawa <matheus_shirakawa@outlook.com>
2022-10-30 04:41:22 -0300

    adding more validations to the csv file and starting to create the user class

--- aula-01mocks/src/file.js
+++ aula-01mocks/src/file.js
@@ -1,5 +1,8 @@
+const { FILE } = require('dns');
 const { readFile } = require('fs/promises')
 const { join } = require('path');
+const { error } = require('./constants')
+
 const DEFAULT_OPTION = {
     maxLines:3,
     fields:["id","name","profession","age"]
@@ -8,23 +11,65 @@ const DEFAULT_OPTION = {
 class File {
     static async csvToJson(filePath){
         const content = await File.getFileContent(filePath)
-        return content
+        const validation = File.isValid(content)
+        if(!validation.valid) throw new Error(validation.error)
+
+        const users = File.parseCSVToJSON(content)
+        return users
     }
 
     static async getFileContent(filePath){
-        const filename = join(__dirname,filePath)
-        return (await readFile(filename)).toString("utf8")
+        return (await readFile(filePath)).toString("utf8")
     }
 
-    // static isValid(csvString, options = )
+    static isValid(csvString, options = DEFAULT_OPTION){
+        const [header, ...fileWithoutHeader] = csvString.replaceAll('\r','').split('\n')
+        const isHeaderValid = header === options.fields.join(',')
 
+        const isContentLengthAccepted = (
+            fileWithoutHeader.length > 0 &&
+            fileWithoutHeader.length <= options.maxLines
+        )
+
+        if(!isHeaderValid){
+            return{
+                error: error.FILE_FIELDS_ERROR_MESSAGE,
+                valid: false,
+            }
+        }
+
+        if(!isContentLengthAccepted){
+            return {
+                error: error.FILE_LENGTH_ERROR_MESSAGE,
+                valid: false,
+                header:header
+            }
+        }
+
+        return { valid: true }
+    }
+
+    static parseCSVToJSON(csvString){
+        const lines = csvString.replaceAll('\r','').split('\n')
+        // remove o primeir item e joga na variavel
+        const firstLine = lines.shift()
+        const header = firstLine.split(',')
+
+        const users = lines.map(line => {
+            const columns = line.split(',')
+            let user = {}
+
+            for(const index in columns){
+                user[header[index]] = columns[index]
+            }
+
+            console.log(user)
+        })
+        // return users
+    }
 }
 
-(async  () => {
-    const result = await File.csvToJson('./../mocks/threeItems-valid.csv');
-    // const result = await File.csvToJson('./../mocks/threeItems-valid.csv');
-    console.log('result',result);
-})();
+module.exports = File
 
 
-// parei nos 18:07
+// parei nos -18:07
---
+++ aula-01mocks/src/user.js
@@ -0,0 +1,11 @@
+class User {
+    constructor({name, id, profession, age}){
+        this.name = name
+        this.id = id
+        this.profession = profession
+        this.age = age
+    }
+
+}
+
+module.exports = User
